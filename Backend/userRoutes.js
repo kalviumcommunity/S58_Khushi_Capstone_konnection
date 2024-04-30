f75136d5800dac737c287e3c3e5d9cf6a12bc71e
unknown <khushi.nain@kalvium.community>
2024-04-30 09:21:40 +0530

    routes

--- Backend/userRoutes.js
+++ Backend/userRoutes.js
@@ -21,3 +21,38 @@ usersRouter.post('/postUsers',(req,res)=>{
   })
 
 module.exports={usersRouter}
+//Create : Add all kalvians data to mongodb
+usersRouter.delete('/postUsers',(req,res)=>{
+  usersModel.insertMany(UsersData)
+  .then((result) => {
+    res.send('Inserted ' + result.length + ' documents into the collection');
+  })
+.catch((error) => {
+   console.error('Error inserting documents:', error);
+   res.status(500).json({ error: 'Failed to insert data' });
+   });
+})
+
+// GET : Display all user data that is in mongodb
+usersRouter.get('/Users/', async (req,res)=>{
+  try {
+    const data= await usersModel.find();
+    res.json(data);
+  } catch (error) {
+    console.log('Error getting the data:', error);
+    res.status(500).json({ error: 'Failed to get the data' });
+  }
+})
+
+// // Delete : Delete all kalvians data
+usersRouter.delete('/deleteUsers', async (req,res)=>{
+  try { 
+      const deleteusers = await usersModel.deleteMany({})
+      res.json(deleteusers)
+  } catch (error) {
+      console.log('Error deleting the data:', error);
+      res.status(500).json({ error: 'Failed to delete the data' });
+  }
+})
+
+module.exports={usersRouter}
